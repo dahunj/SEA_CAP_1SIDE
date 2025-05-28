fbdf03453486a7070a48dca991f8ca416f592a86
DESKTOP-ACFEFB8\USER <dahunj@gmail.com>
2025-05-28 14:00:58 +0900

    version update

--- CME8000/stdafx.h
+++ CME8000/stdafx.h
@@ -69,7 +69,7 @@
 	#pragma comment(lib, "CSGridR.lib")
 #endif
 
-#define MAIN_VERSION	_T("V 1.2.5s")
+#define MAIN_VERSION	_T("V 1.2.10s")
 #define SIM_WAITTIMES	 10		//
 #define SIM_WAITTIMEM	 10		//
 #define ECM_LOG			"D:\\EVMS\\TP\\log\\"

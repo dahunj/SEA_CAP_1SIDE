29c6c8441beef050d28504cbbfbbe1396999a320
ORMPC-20247\Administrator <dahunj@gmail.com>
2025-09-03 22:32:10 +0900

    version

--- CME8000/stdafx.h
+++ CME8000/stdafx.h
@@ -69,7 +69,7 @@
 	#pragma comment(lib, "CSGridR.lib")
 #endif
 
-#define MAIN_VERSION	_T("V 1.2.16s")
+#define MAIN_VERSION	_T("V 1.2.17s")
 #define SIM_WAITTIMES	 10		//
 #define SIM_WAITTIMEM	 10		//
 #define ECM_LOG			"D:\\EVMS\\TP\\log\\"

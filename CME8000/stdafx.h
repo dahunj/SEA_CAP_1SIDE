1cfd3ac3088c84526e64b6e25bcd4ae9bae20833
DESKTOP-ACFEFB8\USER <dahunj@gmail.com>
2025-09-03 22:12:10 +0900

    버전 변경

--- CME8000/stdafx.h
+++ CME8000/stdafx.h
@@ -69,14 +69,14 @@
 	#pragma comment(lib, "CSGridR.lib")
 #endif
 
-#define MAIN_VERSION	_T("V 1.2.13s")
+#define MAIN_VERSION	_T("V 1.2.16s")
 #define SIM_WAITTIMES	 10		//
 #define SIM_WAITTIMEM	 10		//
 #define ECM_LOG			"D:\\EVMS\\TP\\log\\"
 //
-//#define AJIN_BOARD_USE
-//#define LOT_BARCODE_USE
-//#define LOAD_CELL_USE
+#define AJIN_BOARD_USE
+#define LOT_BARCODE_USE
+#define LOAD_CELL_USE
 
 // 테스트 런 옵션
 //#define DRY_RUN_TEST		// Dry Run Test 시 사용
